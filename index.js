15902f249ca59b0291373fed7a75446dab4bc6ba
adambasrie <adamandbasri@gmail.com>
2025-07-31 11:45:11 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const btnEl = document.getElementById("btn");
+const birthdayEl = document.getElementById("birthday");
+const resultEl = document.getElementById("result");
+
+function calculateAge() {
+  const birthdayValue = birthdayEl.value;
+  if (birthdayValue === "") {
+    alert("masukan tanggal lahir anda");
+  } else {
+    const age = getAge(birthdayValue);
+    resultEl.innerText = `Umur anda adalah ${age} ${age > 1 ? "tahun" : "year"} `;
+  }
+}
+
+function getAge(birthdayValue) {
+  const currentDate = new Date();
+  const birthdayDate = new Date(birthdayValue);
+  let age = currentDate.getFullYear() - birthdayDate.getFullYear();
+  const month = currentDate.getMonth() - birthdayDate.getMonth();
+
+  if (
+    month < 0 ||
+    (month === 0 && currentDate.getDate() < birthdayDate.getDate())
+  ) {
+    age--;
+  }
+
+  return age;
+}
+
+btnEl.addEventListener("click", calculateAge);
+
+console.log("hello adam")
