997e31465f45308e1df3410cd0005984f13a47bc
adambasrie <adamandbasri@gmail.com>
2025-07-31 11:48:33 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
 const btnEl = document.getElementById("btn");
-const animeContainerEl = document.querySelector(".anime-container");
-const animeImgEl = document.querySelector(".anime-img");
-const amineNameEl = document.querySelector(".anime-name");
+const birthdayEl = document.getElementById("birthday");
+const resultEl = document.getElementById("result");
 
-btnEl.addEventListener("click", async function () {
-  try {
-    btnEl.disabled = true;
-    btnEl.innerText = "Loading...";
-    amineNameEl.innerText = "Updating...";
-    animeImgEl.src = "spinner.svg";
-    const response = await fetch("https://api.catboys.com/img");
-    const data = await response.json();
-    btnEl.disabled = false;
-    btnEl.innerText = "Get Anime";
-    animeContainerEl.style.display = "block";
-    animeImgEl.src = data.url;
-    amineNameEl.innerText = data.artist;
-  } catch (error) {
-    console.log(error);
-    btnEl.disabled = false;
-    btnEl.innerText = "Get Anime";
-    amineNameEl.innerText = "An error happened, please try again";
+function calculateAge() {
+  const birthdayValue = birthdayEl.value;
+  if (birthdayValue === "") {
+    alert("masukan tanggal lahir anda");
+  } else {
+    const age = getAge(birthdayValue);
+    resultEl.innerText = `Umur anda adalah ${age} ${age > 1 ? "tahun" : "year"} `;
   }
-});
+}
+
+function getAge(birthdayValue) {
+  const currentDate = new Date();
+  const birthdayDate = new Date(birthdayValue);
+  let age = currentDate.getFullYear() - birthdayDate.getFullYear();
+  const month = currentDate.getMonth() - birthdayDate.getMonth();
+
+  if (
+    month < 0 ||
+    (month === 0 && currentDate.getDate() < birthdayDate.getDate())
+  ) {
+    age--;
+  }
+
+  return age;
+}
+
+btnEl.addEventListener("click", calculateAge);
+
+console.log("hello adam")
